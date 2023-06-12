fd6ffa4c486fe15b271f65e0b7fdc524b24da25b
tildeee <simon.desal@adadevelopersacademy.org>
2023-06-12 15:28:04 -0700

    AnimalList component renders each Animal inside an li

--- src/components/AnimalList.js
+++ src/components/AnimalList.js
@@ -5,11 +5,13 @@ import Animal from './Animal';
 const AnimalList = () => {
     return (<section className="AnimalList">
         <h2>Animal List</h2>
-        <Animal></Animal>
-        <Animal></Animal>
-        <Animal></Animal>
-        <Animal></Animal>
-        <Animal></Animal>
+        <ul className="AnimalList__list">
+            <li><Animal></Animal></li>
+            <li><Animal></Animal></li>
+            <li><Animal></Animal></li>
+            <li><Animal></Animal></li>
+            <li><Animal></Animal></li>
+        </ul>
     </section>)
 };
 
