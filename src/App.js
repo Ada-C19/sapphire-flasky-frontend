65bb4d8652e36b50a0b9a932f930d2e8fb5b1fed
tildeee <simon.desal@adadevelopersacademy.org>
2023-06-12 14:59:31 -0700

    creates AnimalList component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 
 import Animal from './components/Animal';
+import AnimalList from './components/AnimalList';
 
 function App() {
   // Comments outside of JSX (but still in JavaScript (aka JS that is not "returned")) can still be //'s.
@@ -9,6 +10,8 @@ function App() {
       <h1>The Sapphire Animal Adoption Agency</h1>
       <h2>Animal Listings</h2>
       {/* // When React renders this JSX, it simply cannot/does not see these slashes as a comment. It will render this as if it wasn't commented */}
+
+      <AnimalList></AnimalList>
       <Animal></Animal>
       <Animal></Animal>
       <Animal></Animal>
---
+++ src/components/AnimalList.js
@@ -0,0 +1,9 @@
+import './AnimalList.css'
+
+const AnimalList = () => {
+    return (<section className="AnimalList">
+        <h2>Animal List</h2>
+    </section>)
+};
+
+export default AnimalList;
