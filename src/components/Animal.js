09c9bddc83df355cb3ebeeee001bfe249afda527
Matt McKnett <matt.mcknett@adadevelopersacademy.org>
2023-06-14 11:14:16 -0700

    Remove "Animal"

--- src/components/Animal.js
+++ src/components/Animal.js
@@ -9,7 +9,7 @@ const Animal = (props) => {
 
     return (
     <section className="Animal">
-        <h3>Animal name: {props.name} (cute!)</h3>
+        <h3>Name: {props.name} (cute!)</h3>
         <p>Species: {props.species}</p>
         <img src="http://placekitten.com/g/200/200" alt="Photo of Willow"></img>
     </section>);
