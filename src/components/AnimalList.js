c3b1bc94188b5c4e11fbcf75fe33fbc209f65284
Matt McKnett <matt.mcknett@adadevelopersacademy.org>
2023-06-14 10:49:42 -0700

    Hard-code a list of animals and render that

--- src/components/AnimalList.js
+++ src/components/AnimalList.js
@@ -3,16 +3,40 @@ import './AnimalList.css'
 import Animal from './Animal';
 
 const AnimalList = () => {
-    return (<section className="AnimalList">
+    const firstAnimal = {
+        name: "Violet",
+        species: "pitbull mix"
+    };
+    const secondAnimal = {
+        name: "Norman",
+        species: "puppy"
+    };
+    const listOfAnimals = [
+        firstAnimal,
+        secondAnimal,
+        {
+            name: "Juni",
+            species: "Poodle"
+        }
+    ];
+
+    const renderedAnimals = listOfAnimals.map(creature => (
+        <li>
+            <Animal
+                name={creature.name}
+                species={creature.species}
+            />
+        </li>
+    ));
+
+    return (
+    <section className="AnimalList">
         <h2>Animal List</h2>
         <ul className="AnimalList__list">
-            <li><Animal></Animal></li>
-            <li><Animal></Animal></li>
-            <li><Animal></Animal></li>
-            <li><Animal></Animal></li>
-            <li><Animal></Animal></li>
+            { renderedAnimals }
         </ul>
-    </section>)
+    </section>
+    )
 };
 
 export default AnimalList;
