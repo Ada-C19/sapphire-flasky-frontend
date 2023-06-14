7a38417b6868a2f768c09ab51e4675815fd383fb
Matt McKnett <matt.mcknett@adadevelopersacademy.org>
2023-06-14 10:40:00 -0700

    Add props to Animal

--- src/components/Animal.js
+++ src/components/Animal.js
@@ -3,12 +3,14 @@ import './Animal.css';
 // The responsibility of this component is to be a reusable UI element that displays an Animal's
 // - name
 // - species
-// - photo
-const Animal = () => {
+// - age
+// - photo (optional?) -- or a default!
+const Animal = (props) => {
 
-    return (<section className="Animal">
-        <h3>Animal name: Samson</h3>
-        <p>Species: Cat</p>
+    return (
+    <section className="Animal">
+        <h3>Animal name: {props.name} (cute!)</h3>
+        <p>Species: {props.species}</p>
         <img src="http://placekitten.com/g/200/200" alt="Photo of Willow"></img>
     </section>);
 };
