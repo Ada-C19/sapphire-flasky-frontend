51325efade3f3e5d15621c607db837a602bd691a
Matt McKnett <matt.mcknett@adadevelopersacademy.org>
2023-06-14 11:56:58 -0700

    Add photo, id, and proptypes

--- src/components/Animal.js
+++ src/components/Animal.js
@@ -1,4 +1,5 @@
 import './Animal.css';
+import PropTypes from 'prop-types';
 
 // The responsibility of this component is to be a reusable UI element that displays an Animal's
 // - name
@@ -9,10 +10,18 @@ const Animal = (props) => {
 
     return (
     <section className="Animal">
-        <h3>Name: {props.name} (cute!)</h3>
-        <p>Species: {props.species}</p>
-        <img src="http://placekitten.com/g/200/200" alt="Photo of Willow"></img>
+        <h3>Name: { props.name } (cute!)</h3>
+        <p>Species: { props.species }</p>
+        { props.photo ? <img src={ props.photo } alt="Photo of Willow"></img> : "[No Photo]" }
     </section>);
 };
+
+Animal.propTypes = {
+    name: PropTypes.string,
+    species: PropTypes.string.isRequired,
+    adopted: PropTypes.bool,
+    age: PropTypes.number,
+    photo: PropTypes.string
+}
 
 export default Animal;
--- src/components/AnimalList.js
+++ src/components/AnimalList.js
@@ -1,32 +1,24 @@
 import './AnimalList.css'
+import PropTypes from 'prop-types';
 
 import Animal from './Animal';
 
-const AnimalList = () => {
-    const listOfAnimals = [
-        {
-            name: "Violet",
-            species: "pitbull mix"
-        },
-        {
-            name: "Norman",
-            species: "puppy"
-        },
-        {
-            name: "Juni",
-            species: "Poodle"
-        }
-    ];
+const AnimalList = ({ listOfAnimals }) => {
+    // If the parameter is props instead, you can do these!
+    // const listOfAnimals = props.listOfAnimals;
+    // const { listOfAnimals } = props;
 
     return (
     <section className="AnimalList">
         <h2>Animal List</h2>
         <ul className="AnimalList__list">
             {
-                listOfAnimals.map(creature => (<li>
+                listOfAnimals.map((creature) => (
+                    <li key={creature.id}>
                         <Animal
                             name={ creature.name }
                             species={ creature.species }
+                            photo={ creature.photo }
                         />
                     </li>)
                 ) 
@@ -35,5 +27,18 @@ const AnimalList = () => {
     </section>
     );
 };
+
+AnimalList.propTypes = {
+    listOfAnimals: PropTypes.arrayOf(
+        PropTypes.shape({
+            name: PropTypes.string,
+            id: PropTypes.number.isRequired,
+            species: PropTypes.string.isRequired,
+            adopted: PropTypes.bool,
+            age: PropTypes.number,
+            photo: PropTypes.string
+        })
+    )
+}
 
 export default AnimalList;
