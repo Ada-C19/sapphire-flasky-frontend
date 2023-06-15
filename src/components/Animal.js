bc221926baf2dec562ad0f9d67901d9dec42ec20
tildeee <simon.desal@adadevelopersacademy.org>
2023-06-15 13:49:39 -0700

    cleans up comments, etc

--- src/components/Animal.js
+++ src/components/Animal.js
@@ -3,45 +3,16 @@ import PropTypes from 'prop-types';
 
 import { useState } from 'react';
 
-// [X] Make the element that the user interacts with
-// [X] Make the event handler for that element
-// [X] Configure a piece of state:
-    // Decide what the state is... what is its name, what is its type, what are the available values for this
-        // isBookmarked ... true or false
-        // likesCount ... numbers 0+
-        // biography ... {birthYear:, works:}
-    // import useState
-    // Render the piece of state with an initial value
-        // setIsBookmarked
-        // false
-    // Make the event handler update the state
-// [X] Test it
-// [X] Refactor
-// [] Style it/add polish
-
-
-
-// Refactor to toggle:
-// Rename my functions
-// Redo the logic
-
-// The responsibility of this component is to be a reusable UI element that displays an Animal's
 const Animal = (props) => {
 
     const [isBookmarked, setIsBookmarked] = useState(false);
 
-    const altText = `Photo of ${props.name}`;
-
-    // Responsibility:
-    // Event Handler
-    // Update the state
     const toggleBookmark = () => {
         setIsBookmarked(!isBookmarked);
     }
 
-    // If the animal is bookmarked, the CSS class name should be
-    //   .bookmarked
-    // If the animal is not bookmarked, there should be no CSS class
+    const altText = `Photo of ${props.name}`;
+
     let animalStyle = 'Animal';
     if (isBookmarked) {
         animalStyle = 'Animal bookmarked';
@@ -54,7 +25,6 @@ const Animal = (props) => {
         <h3>Name: { props.name }</h3>
         <p>Species: { props.species }</p>
         <button onClick={toggleBookmark} className="bookmark-button">🌟 Bookmark</button>
-
     </section>);
 };
 
